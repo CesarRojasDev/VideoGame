e6dc22622e76d60ec5d71f11e3d5cb89ef9ddad5
CTurkowsky <91448665+CTurkowsky@users.noreply.github.com>
2022-06-09 16:23:01 -0500

    Correccion de scripts Level
    Se corrigio los scipsts localizados en Level

--- Assets/Scripts/Level/PlayerSpawner.cs
+++ Assets/Scripts/Level/PlayerSpawner.cs
@@ -3,51 +3,57 @@ using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
 {
-	[Header("Dependencies")]
-	public PlayerPathSO playerPath;
-	public GameObject playerPrefab;
-	public CinemachineVirtualCamera followCamera;
-	public GameObject playerParent;
+    [Header("Dependencies")]
+    public PlayerPathSO playerPath;
+    public GameObject playerPrefab;
+    public CinemachineVirtualCamera followCamera;
+    public GameObject playerParent;
 
-	public void InstantiaterPlayerOnLevel()
-	{
-		GameObject player = GetPlayer();
-		Transform entrance = GetLevelEntrance(playerPath.levelEntrance);
+    public void InstantiatePlayerOnLevel()
+    {
+        GameObject player = GetPlayer();
+        Transform entrance = GetLevelEntrance(playerPath.levelEntrance);
 
-		player.transform.position = entrance.transform.position;
-		player.transform.parent = playerParent.transform;
-		this.followCamera.Follow = player.transform;
+        player.transform.position = entrance.transform.position;
+        player.transform.parent = playerParent.transform;
+        this.followCamera.Follow = player.transform;
 
-		playerPath.leavelEntrance = null;
+        // When player is instantiated and moved, reset path
+        playerPath.levelEntrance = null;
+    }
 
-	}
-	private GameObject GetPlayer()
-	{
-		GameObject playerObject = GameObject.findGameObjectWithTag("Player");
+    private GameObject GetPlayer()
+    {
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
 
-		if(playerObject == null)
-		{
-			playerObject = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
-		}
-		return playerObject;
-	}
+        if (playerObject == null)
+        {
+            // No player found
+            playerObject = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        }
 
-	private Transform GetLevelEntrance(LevelEntranceSO playerEntrance)
-	{
-		if (playerEntrance == null)
-		{
-			return this.transform.GetChild(0).transform;
-		}
+        return playerObject;
+    }
 
-	var levelEntrances = GameObject.FindObjectsOfType<LevelEntrance>();
-	foreach (LevelEntrance levelEntrance in  levelEntrances)
-	{
-		if(levelEntrance.entrance == playerEntrance)
-		{
-			return levelEntrance.gameObject.transform;
-		}
-	}
-		return this.transform.GetChild(0).transform;
-	}
+    private Transform GetLevelEntrance(LevelEntranceSO playerEntrance)
+    {
+        if (playerEntrance == null)
+        {
+            // No path for player, instantiate it at default position
+            return this.transform.GetChild(0).transform;
+        }
+
+        var levelEntrances = GameObject.FindObjectsOfType<LevelEntrance>();
+
+        foreach (LevelEntrance levelEntrance in levelEntrances)
+        {
+            if (levelEntrance.entrance == playerEntrance)
+            {
+                return levelEntrance.gameObject.transform;
+            }
+        }
+
+        // No entrance found, return default
+        return this.transform.GetChild(0).transform;
+    }
 }
-
--- Assets/Scripts/Level/Sceneinitializer.cs
+++ Assets/Scripts/Level/Sceneinitializer.cs
@@ -1,35 +1,39 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-public class Sceneinitializer : MonoBehaviour
+public class SceneInitializer : MonoBehaviour
 {
-	[Header("Dependencies")]
-	public SceneSO[] sceneDependencies;
-	[Header("On Scene Ready")]
-	public UnityEvent onDependenciesLoaded;
+    [Header("Dependencies")]
+    public SceneSO[] sceneDependencies;
 
-	void Start()
-	{
-		StartCoroutine(LoadDependencies());
-	}
-	private IEnumerator LoadDependencies()
-	{
-		for (int i = 0; i <= this.sceneDependencies.Length - 1; i++)
-		{
-			SceneSO sceneToLoad = this.sceneDependencies[i];
-			if(SceneManager.GetSceneByName(sceneToLoad.name).isLoaded == false)
-			{
-				var loadOperation = SceneManager.LoadSceneAsync(sceneToLoad.name, LoadSceneMode.Additive);
-				while (loadOperation.isDone == false)
-				{
-					yield return null;
-				}
-			}
-		}
-		if (onDependenciesLoaded != null)
-			onDependenciesLoaded.Invoke();
-	}
+    [Header("On Scene Ready")]
+    public UnityEvent onDependenciesLoaded;
+
+    void Start()
+    {
+        StartCoroutine(LoadDependencies());
+    }
+
+    private IEnumerator LoadDependencies()
+    {
+        for (int i = 0; i <= this.sceneDependencies.Length - 1; i++)
+        {
+            SceneSO sceneToLoad = this.sceneDependencies[i];
+
+            if (SceneManager.GetSceneByName(sceneToLoad.name).isLoaded == false)
+            {
+                var loadOperation = SceneManager.LoadSceneAsync(sceneToLoad.name, LoadSceneMode.Additive);
+
+                while (loadOperation.isDone == false)
+                {
+                    yield return null;
+                }
+            }
+        }
+
+        if (onDependenciesLoaded != null)
+            onDependenciesLoaded.Invoke();
+    }
 }
